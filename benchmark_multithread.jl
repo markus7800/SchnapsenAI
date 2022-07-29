37911da6dabddedc030eb3dbf1a6d89a89456209
Markus Boeck <markus.boeck7800@hotmail.com>
2022-07-29 22:11:43 +0200

    bench

---
+++ benchmark_multithread.jl
@@ -0,0 +1,25 @@
+include("game2.jl")
+
+@info "Benchmark Multithread:" Threads.nthreads()
+
+g = Game(0)
+play_move!(g, stringtomove("10♣ t")) # 10♣ t: 0.5784 1000 (AB: K♣ t 2000, 10♡ 2000)
+play_move!(g, stringtomove("A♣")) # A♣: 0.672 1000 (A♣: 1000.0)
+
+Random.seed!(0)
+@time eval_moves_prob(g, 2500)
+
+#julia --project=. --threads=1 benchmark_multithread.jl
+
+# Q♣: 0.7428 ± 0.0087 
+# 10♠: 0.9048 ± 0.0059
+# J♢: 0.7024 ± 0.0091 *
+# Q♢: 0.7712 ± 0.0084
+# A♡: 0.8744 ± 0.0066
+
+# macbook
+# n_threads, % CPU, User load, time
+# 1, 99, 28%, 2:04
+# 2, 160, 44%, 1:25
+# 4, 250, 65%, 1:09
+# 8, 300, 80%, 1:02
